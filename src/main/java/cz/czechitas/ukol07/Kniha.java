2cef969feeb48902b5471c235b4a6a1a0aa266cd
zdeni sykorova <sykorova@hotmail.com>
2025-06-20 15:14:46 +0200

    třída Kniha

--- src/main/java/cz/czechitas/ukol07/Kniha.java
+++ src/main/java/cz/czechitas/ukol07/Kniha.java
@@ -1,4 +1,31 @@
 package cz.czechitas.ukol07;
 
 public class Kniha {
+    private String autor;
+    private String nazev;
+    private int rokVydani;
+
+    public Kniha(String autor, String nazev, int rokVydani) {
+        this.autor = autor;
+        this.nazev = nazev;
+        this.rokVydani = rokVydani;
+    }
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    public String getNazev() {
+        return nazev;
+    }
+    public void setNazev(String nazev) {
+        this.nazev = nazev;
+    }
+    public int getRokVydani() {
+        return rokVydani;
+    }
+    public void setRokVydani(int rokVydani) {
+        this.rokVydani = rokVydani;
+    }
 }
